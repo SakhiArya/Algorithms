7d943aa1f46b4c93f50c5c6805a963a14908a684
SakhiArya <sakhiarya009@gmail.com>
2017-07-30 13:17:25 +0530

    Update CountingSort.java

--- Sorting/src/sorting/CountingSort.java
+++ Sorting/src/sorting/CountingSort.java
@@ -21,18 +21,22 @@ public class CountingSort {
 	}
 	public static void countSort(int[] ipArr,int min,int max)
 	{
+		//creates the array with the required size
 		int countArr[] =new int[max-min+1];
 		int opArr[] = new int[ipArr.length];
+		//initialize the array elements with zero
 		Arrays.fill(countArr, 0);
+		//count the number of occurences of each element
 		for(int i = 0; i <ipArr.length;i++)
 		{
 			++countArr[ipArr[i]-min];
 		}
+		//creates the index for each element by cumilation
 		for(int i=1;i<countArr.length;i++)
 		{
 			countArr[i] += countArr[i-1];
 		}
-
+                //process the i/p array for output
 		for(int i = 0 ;i <ipArr.length;i++)
 		{
 			int indexForCount=ipArr[i]-min;
